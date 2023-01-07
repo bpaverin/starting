d376397fecd0fb6261232fb72bdcc0111a3e4e4e
s1tcom <bpaverin@gmail.com>
2023-01-07 16:21:17 +0300

    start learn for

---
+++ start.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+
+	/*i := 5
+	for i <= 10 {
+		fmt.Println(i)
+		i = i + 1
+	}
+
+	for i := 0; i <= 5; i++ {
+		fmt.Println(i)
+	}
+
+	text := []string{"borya", "privet", "go", "code"}
+	fmt.Println(text[3])
+	for _, i := range text {
+		if i == "go" {
+			continue
+		}
+		fmt.Println(i)
+	}*/
+
+	/*for {
+		fmt.Println("lol")
+	}*/
+
+	files, err := ioutil.ReadDir(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		fmt.Println(file.Name())
+	}
+}
